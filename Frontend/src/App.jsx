8fbfab3b88ddccd1239200564a3fbeafd41bfb34
arpitasen20 <arpitasen200311@gmail.com>
2024-09-27 23:01:56 +0530

    done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Dashboard from './Dashboard'
+import React from 'react';
+import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Landing from './Landing/Landingpg';
+import Home from './Home/Home';
+import Dashboard from './Home/Dashboard';
+import Topbar from './Home/Topbar';
+import Sidebar from './Home/Sidebar';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <Dashboard/>
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;

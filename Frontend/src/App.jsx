f1c43833cd9194bec719f2b9f926238c99eb8c61
arpitasen20 <arpitasen200311@gmail.com>
2024-09-28 21:21:05 +0530

    done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,18 +4,27 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './Landing/Landingpg';
 import Home from './Home/Home';
 import Dashboard from './Home/Dashboard';
-import Topbar from './Home/Topbar';
-import Sidebar from './Home/Sidebar';
 import Project from './Home/Project';
+import Financials from './Home/Financials';
+import Documents from './Home/Documents';
+import Reports from './Home/Reports';
+import Help from './Home/Help';
+import Settings from './Home/Settings';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Landing /> }/>
+        <Route path="/home" element={ <Home />}/>
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/project" element={<Project />} />
+        <Route path="/project" element={<Project />}/>
+        <Route path="/financials" element={<Financials />}/>
+        <Route path="/documents" element={<Documents />}/>
+        <Route path="/reports" element={<Reports />}/>
+        <Route path="/help" element={<Help />}/>
+        <Route path="/settings"element={<Settings />}/>
+        {/* <Route path="/signout" element={<Signout />} /> */}
       </Routes>
     </Router>
   );
